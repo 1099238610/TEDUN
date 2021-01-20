0fbde6a4377228b3a2da500bc9c48dbe512fb606
Gao <1099238610@qq.com>
2021-01-20 10:29:43 +0800

    add alibaba logo and made-in-china logo

---
+++ js/breadcrumb.js
@@ -0,0 +1,16 @@
+
+function createBread(){
+
+    let div = document.createElement("div")
+    let navbar = document.createElement("nav")
+    let ol = document.createElement("ol")
+    let li = document.createElement("li")
+
+    div.className += "container-fluid w-75"
+    navbar.setAttribute("aria-label","breadcrumb")
+    ol.className += "breadcrumb"
+    li.className += "breadcrumb-item"
+
+    
+
+}
